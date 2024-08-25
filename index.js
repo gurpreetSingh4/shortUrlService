709f13d31426737e1ba3a399abb6891eaa42c041
gurpreetSingh4 <gurpreetyarasingh@gmail.com>
2024-08-25 17:09:52 +0530

    server side rendering using EJS

--- index.js
+++ index.js
@@ -1,17 +1,43 @@
-const express = require("express")
-const {connectToMongoDB} = require("./connect")
-const URL = require('./models/url')
+const express = require("express");
+const path = require('path');
+const {connectToMongoDB} = require("./connect");
+const URL = require('./models/url');
 const urlRoute = require('./routes/url');
+const staticRoute = require('./routes/staticRouter')
 const app = express();
 const PORT = 8001;
 
 connectToMongoDB('mongodb://127.0.0.1:27017/short-url').then( ()=> console.log('MongoDB connnected'));
 
+app.set('view engine', 'ejs');
+app.set('views', path.resolve('./views'))
+
 app.use(express.json());
+app.use(express.urlencoded({extended: false}))
 
 app.use('/url', urlRoute);
+app.use('/', staticRoute)
+ 
+// server side rendering (templating engins : EJS, pug js, handlebars )
+app.get("/test", async (req, res) => {
+    const allUrls = await URL.find({})
+    return res.render('home',{
+        urls: allUrls,
+    })
+    // return res.end(`
+    //     <html>
+    //         <head>
+    //             <body>
+    //                 <ol>
+    //                     ${allUrls.map(url => `<li>${url.shortId} -> ${url.redirectURL} -> ${url.visitHistory.length}</li>`).join('')}
+    //                 </ol>
+    //             </body>
+    //         </head>
+    //     </html>
+    //     `)
+})
 
-app.get('/:shortId', async(req, res) => {
+app.get('/url/:shortId', async(req, res) => {
     const shortId = req.params.shortId;
     const entry =  await URL.findOneAndUpdate({
         shortId
