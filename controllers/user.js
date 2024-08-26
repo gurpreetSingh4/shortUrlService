4f04661b33c1373b21c7403f5d39cfb0fd804899
gurpreetSingh4 <gurpreetyarasingh@gmail.com>
2024-08-26 14:49:25 +0530

    JWT authentication stateless"

--- controllers/user.js
+++ controllers/user.js
@@ -21,9 +21,8 @@ async function handleUserLogin(req, res){
     if(!user) return res.render("home", {
         error: "Invalid Username or Password",
     })
-    const sessionId = uuidv4();
-    setUser(sessionId, user);
-    res.cookie('uid', sessionId)
+    const token = setUser(user);
+    res.cookie('uid', token)
     return res.redirect("/")
 }
 
